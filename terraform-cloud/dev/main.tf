2059b3a53d97b5bc8e612a0f6787ea9aa35f79c6
Jaemin Seo <86sjmjm@gmail.com>
2021-03-30 00:46:57 +0900

    Example codes for terraform cloud
    Signed-off-by: Jaemin Seo <86sjmjm@gmail.com>

---
+++ terraform-cloud/dev/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "dev"
+  }
+}
